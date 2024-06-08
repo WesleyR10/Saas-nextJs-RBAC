364f17003b42b50cdf46e53ecec9ce0dca7e17e1
WesleyR10 <wesleyribas2015@gmail.com>
2024-06-08 04:39:17 -0300

    Getting the authenticated user

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,16 +1,7 @@
-'use client'
+import { auth } from '@/auth/auth'
 
-import { Separator } from '@radix-ui/react-separator'
+export default async function Home() {
+  const { user } = await auth()
 
-import { Button } from '@/components/ui/button'
-
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to the Web App</h1>
-      <Button className="my-2 ">Click me</Button>
-      <Separator className="my-2 h-px w-auto bg-slate-100" />
-      <p className="text-slate-500">This is a paragraph</p>
-    </div>
-  )
+  return <pre>{JSON.stringify(user, null, 2)}</pre>
 }
