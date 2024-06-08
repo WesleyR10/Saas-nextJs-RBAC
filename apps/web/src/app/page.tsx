61b2f05bd3b6e93af444b8319094f13d30703000
WesleyR10 <wesleyribas2015@gmail.com>
2024-06-08 10:18:57 -0300

    Setup project Shadcn-ui

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,16 @@
+'use client'
+
+import { Separator } from '@radix-ui/react-separator'
+
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <h1>Welcome to the Web App</h1>
+      <Button className="my-2 ">Click me</Button>
+      <Separator className="my-2 h-px w-auto bg-slate-100" />
+      <p className="text-slate-500">This is a paragraph</p>
+    </div>
+  )
 }
