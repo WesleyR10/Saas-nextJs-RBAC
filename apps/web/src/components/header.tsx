7a895cb515d85f19bde5c0ac289833547f61ae5a
WesleyR10 <wesleyribas2015@gmail.com>
2024-06-10 19:35:32 -0300

    Action create organization

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -13,7 +13,7 @@ export async function Header() {
   const permissions = await ability()
 
   return (
-    <div className="mx-auto flex max-w-[1200px] items-center justify-between">
+    <div className="mx-auto flex max-w-[1200px] items-center justify-between border-b pb-2">
       <div className="flex items-center gap-3">
         <Image
           src={rocketseatIcon}
@@ -27,7 +27,6 @@ export async function Header() {
 
         {permissions?.can('get', 'Project') && <p>Project</p>}
       </div>
-
       <div className="flex items-center gap-4">
         <ThemeSwitcher />
         <Separator orientation="vertical" className="h-5" />
